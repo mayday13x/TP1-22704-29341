b7e16453fe9aa671c3264431e1ac255996603692
Miguel Silva <mjosea@ipvc.pt>
2024-03-28 17:22:32 +0000

    add mov player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,13 @@ public class PlayerScript : MonoBehaviour
     public Rigidbody rb;
 
     public float forwardForce = 100f;
-    public float sidewaysForce = 50f;
+    //public float sidewaysForce = 50f;
+
+    bool Lane1 = false;
+    bool Lane2 = true;
+    bool Lane3 = false;
+
+    public Transform Player;
 
 
     void Start()
@@ -21,14 +27,49 @@ public class PlayerScript : MonoBehaviour
 
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
+    
 
-        if (Input.GetKey("d"))
+        if (Input.GetKeyDown(KeyCode.D) && Lane3 == false && Lane2 == false && Lane1 == true)
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            Lane2 = true;
+            Lane1 = false;
+            Lane3 = false;
+
+            Vector3 cpos = Player.transform.position;
+            cpos.x = -13.5f;
+            Player.position = cpos;
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && Lane2 == true)
+        {
+            Lane1 = true;
+            Lane2 = false;
+            Lane3 = false;
+
+            Vector3 cpos = Player.transform.position;
+            cpos.x = -20.7f;
+            Player.position = cpos;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && Lane2 == true)
+        {
+            Lane3 = true;
+            Lane1 = false;
+            Lane2 = false;
+
+            Vector3 cpos = Player.transform.position;
+            cpos.x = -6.4f;
+            Player.position = cpos;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && Lane1 == false && Lane3 == true)
+        {
+            Lane2 = true;
+            Lane1 = false;
+            Lane3 = false;
+
+            Vector3 cpos = Player.transform.position;
+            cpos.x = -13.5f;
+            Player.position = cpos;
         }
 
     }

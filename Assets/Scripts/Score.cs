8629622ff08093296b107ec0992bd5a9bd5fa924
Miguel Silva <mjosea@ipvc.pt>
2024-04-18 19:29:14 +0100

    ScoreText OK

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
     float totalTime;
-    float ScoreRate = 0.03f;
+    float ScoreRate = 0.01f;
     public float Score_;
-  //  public Rigidbody rb;
 
-    // Update is called once per frame
+    public Text ScoreText;
+
     void Update()
     {
+        Debug.Log(123);
+      //  ScoreText.text = "Score: " + Score_;
         if (this.GetComponent<Animator>().GetBool("isRunning"))
         {
             totalTime += Time.deltaTime;
             Score_ += totalTime * ScoreRate;
+            ScoreText.text = "Score: " + Mathf.Round(Score_);
+
         }
 
     }
